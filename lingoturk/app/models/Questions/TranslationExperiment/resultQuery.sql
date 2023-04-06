d377593b85995da3b33a7636de2501356e4f563d
jeslynlamxy <jeslynlxy@hotmail.com>
2023-04-06 20:46:32 +0800

    small updates

--- lingoturk/app/models/Questions/TranslationExperiment/resultQuery.sql
+++ lingoturk/app/models/Questions/TranslationExperiment/resultQuery.sql
@@ -1,1 +1,7 @@
-SELECT * FROM TranslationResults
+SELECT fileName, listNumber, assignmentId, hitId, workerId, origin, timestamp, partId, questionId, answer::TEXT, (data->>'Before') as Before, (data->>'After') as After, (data->>'Improvements') as Improvements, id FROM (
+	(SELECT * FROM Results WHERE experimentType='TranslationExperiment') as tmp1
+	LEFT OUTER JOIN Questions USING (QuestionId)
+	LEFT OUTER JOIN Groups USING (PartId)
+) as tmp
+WHERE LingoExpModelId = 4
+ORDER BY partId, questionId, workerId
